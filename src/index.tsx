212b84f513898d814aa21f3eac2b322783c4e056
Dmitry Butakov <sportyounde@MacBook-AirDmitry.local>
2022-08-19 21:47:32 +0400

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import {Provider} from "react-redux";
 import {store} from "./02_BLL/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
